a500906f60ac57796129b8797917601f3f2fe80f
Paco Moreno <fjarana@itesm.mx>
2018-04-20 19:38:03 -0500

    Add tokens servlet, currently empty

---
+++ src/main/java/mx/nic/rdap/auth/openidc/servlet/TokensServlet.java
@@ -0,0 +1,21 @@
+package mx.nic.rdap.auth.openidc.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TokensServlet extends HttpServlet {
+
+	/**
+	 * Serial version
+	 */
+	private static final long serialVersionUID = -3755516672887712201L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// FIXME Complete..
+	}
+
+}
